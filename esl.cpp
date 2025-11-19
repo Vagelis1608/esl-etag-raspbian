a63d68a3e6d6b636e7b5e8d0c173129eea63d212
Vagelis1608 <Vagelis1608@users.noreply.github.com>
2025-11-20 00:03:29 +0200

    Initial data struct

---
+++ esl.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <sys/sysinfo.h>
+#include <fstream>
+
+std::string runCmd(const std::string& command) {
+    char psBuffer[256];
+    FILE*   pPipe;
+    std::string result;
+
+    if ((pPipe = popen(command.c_str(), "rt")) == NULL) {
+        std::cerr << "Cannot start command\n";
+        return "";
+    }
+
+    while (fgets(psBuffer, sizeof(psBuffer), pPipe) != NULL) 
+        result += psBuffer;
+
+    if(!feof(pPipe)) std::cerr << "Error executing command\n";
+
+    pclose(pPipe);
+    return result;
+}
+
+struct systemData {
+    int temperature;
+    long totalram, freeram, load[3];
+    std::string localIP, uptime, memunit;
+
+    bool init() {
+        struct sysinfo info;
+        if ( sysinfo( &info ) != 0 ) return false;
+
+        this->totalram = info.totalram;
+
+        this->freeram = info.freeram;
+        
+        for ( int i=0; i < 3; i++ ) this->load[i] = info.loads[i]; // 1, 5, 15 minute loads
+
+        this->memunit = ' B';
+        if ( info.mem_unit == 1024 ) this->memunit = 'KB';
+        if ( info.mem_unit == ( 1024 * 1024 ) ) this->memunit = 'MB';
+        if ( info.mem_unit == ( 1024 * 1024 * 1024 ) ) this->memunit = 'GB';
+        if ( info.mem_unit == ( 1024 * 1024 * 1024 * 1024 ) ) this->memunit = 'TB'; // How?
+
+        unsigned long rounded = ( info.uptime / 60 ) + 1;
+        this->uptime = ( rounded % 60 ) + "mins";
+        rounded /= 60;
+        if ( rounded > 0 ) {
+            this->uptime = ( rounded % 24 ) + "hours, " + this->uptime;
+            rounded /= 24;
+
+            if ( rounded > 0 ) this->uptime = rounded + "days, " + this->uptime;
+        }
+
+        // Get temperature and round it
+        std::string reportedTemp = runCmd( "cat /sys/class/thermal/thermal_zone0/temp" );
+        try {
+            this->temperature = std::stoi(reportedTemp) / 1000;
+        } catch (const std::exception& e) {
+            this->temperature = -274; // Just below absolute zero, fairly clear that something went wrong XD
+        }
+        
+        this->localIP = runCmd( "hostname -I | cut -d' ' -f1" );
+
+        return true;
+    }
+};
